1426bca942ac98aaff7e008cc8bcacb4d7dd8e6f
Iwan Morgan <ibmorgan0@gmail.com>
2016-11-25 11:24:37 +0000

    Update content.js

--- public/js/content.js
+++ public/js/content.js
@@ -5,7 +5,7 @@ var Home = {
 }
 
 var About = {
-	title: '<h1>We are THamesVale TaeKwonDo</h1>',
+	title: '<h1>We are TaeKwonDo</h1>',
 	image: '<img src="http://placehold.it/750x750"/>',
-	body: '<p>We are a club situated in the Cotswolds, we have three clubs.</p>'
+	body: '<p></p>'
 }
